25eb84b9f47c59e8dbe68ab46b6340256b6b7151
Matthew Hoggan <mehoggan@gmail.com>
2015-09-15 18:32:09 -0700

    Adding curves, and adding in broken rendering in functions.
    Change-Id: I49f4a7731f56735bfbcdf59d4d24a761677ebbb6

--- warscrollrelationsgraph.cpp
+++ warscrollrelationsgraph.cpp
@@ -2,67 +2,105 @@
 
 #include <iostream>
 
+const std::atomic_bool t{true};
+const std::atomic_bool f{false};
+
 WarScrollRelationsGraph::WarScrollRelationsGraph() :
-  m_draw(false),
   m_t(0),
-  m_program(0)
-{}
+  m_program(nullptr)
+{
+  m_draw = f;
+  m_create = f;
+  m_initialize = t;
+}
 
 WarScrollRelationsGraph::~WarScrollRelationsGraph()
 {
   delete m_program;
+  m_program = nullptr;
+  m_initialize = t;
+
+  m_draw = f;
+  m_create = f;
+}
+
+void WarScrollRelationsGraph::renderBackground()
+{
+  m_program->bind();
+  m_program->enableAttributeArray(0);
+  float values[] = {1, -1, 1, -1, -1, 1, 1, 1};
+  m_program->setAttributeArray(0, GL_FLOAT, values, 2);
+  m_program->setUniformValue("t", (float) m_t);
+  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+  m_program->disableAttributeArray(0);
+  m_program->release();
+}
+
+
+void WarScrollRelationsGraph::renderGraph()
+{
+}
+
+void WarScrollRelationsGraph::creatStaticData()
+{
+  initializeOpenGLFunctions();
+
+  if (m_program) {
+    delete m_program;
+    m_program = nullptr;
+  }
+
+  m_program = new QOpenGLShaderProgram();
+  m_program->addShaderFromSourceCode(QOpenGLShader::Vertex,
+    "attribute highp vec4 vertices;"
+    "varying highp vec2 coords;"
+    "void main() {"
+    "    gl_Position = vertices;"
+    "    coords = vertices.xy;"
+    "}");
+
+  m_program->addShaderFromSourceCode(QOpenGLShader::Fragment,
+    "uniform lowp float t;"
+    "varying highp vec2 coords;"
+    "void main() {"
+    "  gl_FragColor = vec4(0.0, 0.0, 0.0, 0.4);"
+    "}");
+
+  m_program->bindAttributeLocation("vertices", 0);
+  m_program->link();
+}
+
+void WarScrollRelationsGraph::createGraph()
+{
+}
+
+void WarScrollRelationsGraph::setGraph(WarScrollSynergyGraph &graph)
+{
+  std::lock_guard<std::mutex> lock(m_graphMutex);
+  m_graph = &graph;
+  m_create = t;
 }
 
 void WarScrollRelationsGraph::paint()
 {
-  if (!m_program) {
-    initializeOpenGLFunctions();
+  if (m_initialize) {
+    creatStaticData();
+    m_initialize = f;
+  }
 
-    m_program = new QOpenGLShaderProgram();
-    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex,
-      "attribute highp vec4 vertices;"
-      "varying highp vec2 coords;"
-      "void main() {"
-      "    gl_Position = vertices;"
-      "    coords = vertices.xy;"
-      "}");
-
-    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment,
-      "uniform lowp float t;"
-      "varying highp vec2 coords;"
-      "void main() {"
-      "  gl_FragColor = vec4(0.0, 0.0, 0.0, 0.4);"
-      "}");
-
-    m_program->bindAttributeLocation("vertices", 0);
-    m_program->link();
+  if (m_create) {
+    createGraph();
+    m_create = f;
   }
 
   if (m_draw) {
-    m_program->bind();
-
-    m_program->enableAttributeArray(0);
-
-    float values[] = {
-      -1, -1,
-      1, -1,
-      -1, 1,
-      1, 1
-    };
-    m_program->setAttributeArray(0, GL_FLOAT, values, 2);
-    m_program->setUniformValue("t", (float) m_t);
-
     qreal width = m_viewportSize.width();
     qreal height = m_viewportSize.height();
 
-    glViewport(0, 0.10 * height, width, height);
-
+    glViewport(0, 0.11 * height, width, height);
     glDisable(GL_DEPTH_TEST);
-
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-    m_program->disableAttributeArray(0);
-    m_program->release();
+    renderBackground();
+    renderGraph();
   }
 }
 
--- warscrollrelationsgraph.h
+++ warscrollrelationsgraph.h
@@ -5,12 +5,33 @@
 #include <QtGui/QOpenGLShaderProgram>
 #include <QtGui/QOpenGLFunctions>
 
+#include "warscrollsynergygraph.h"
+
 #include <atomic>
+#include <mutex>
 
 class WarScrollRelationsGraph :
   public QObject, public QOpenGLFunctions
 {
   Q_OBJECT
+
+private:
+  std::atomic_bool m_draw;
+  std::atomic_bool m_create;
+  std::atomic_bool m_initialize;
+  QSize m_viewportSize;
+  qreal m_t;
+  QOpenGLShaderProgram *m_program;
+
+  std::mutex m_graphMutex;
+  WarScrollSynergyGraph *m_graph;
+
+private:
+  void renderBackground();
+  void renderGraph();
+  void creatStaticData();
+  void createGraph();
+
 public:
   WarScrollRelationsGraph();
   ~WarScrollRelationsGraph();
@@ -18,15 +39,10 @@ public:
   void setT(qreal t) {m_t = t;}
   void setViewportSize(const QSize &size) {m_viewportSize = size;}
   void setDraw(bool draw) {m_draw = draw;}
+  void setGraph(WarScrollSynergyGraph &graph);
 
 public slots:
   void paint();
-
-private:
-  std::atomic_bool m_draw;
-  QSize m_viewportSize;
-  qreal m_t;
-  QOpenGLShaderProgram *m_program;
 };
 
 #endif // WARSCROLLRELATIONSGRAPH_H
